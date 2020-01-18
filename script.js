4d2c8c5aa123dfeeb3def25c3ae688a64c062136
Muhammad Ismail <47635180+ismardoi2001@users.noreply.github.com>
2020-01-19 00:20:57 +0500

    J Query Update  calculator

--- script.js
+++ script.js
@@ -9,3 +9,18 @@ function sum(a, b){
 //sample usage
 console.log(sum(1, 3));
 console.log(sum(2, 5));
+// JQuery Calculator  Code here 
+function insert(num){
+    $('.calc-display').val($('.calc-display').val() + num)
+
+}
+function eql(){
+    $('.calc-display').val(eval($('.calc-display').val()));
+}
+function c(){
+    $('.calc-display').val('');
+}
+function del(){
+    value = $('.calc-display').val();
+    $('.calc-display').val(value.substring(0,value.length  -1));
+}
